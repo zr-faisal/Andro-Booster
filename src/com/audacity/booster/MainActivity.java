1d14377bbf17af6d05ded511716c598d6800bec0
zr-faisal <zr.faisal@programmer.net>
2013-07-11 13:52:19 +0900

    Navigation Drawer added!

--- src/com/audacity/booster/MainActivity.java
+++ src/com/audacity/booster/MainActivity.java
@@ -1,23 +1,44 @@
 package com.audacity.booster;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
+	ListView lvNavDrawer;
 	TextView tvMemoryBooster, tvSignalBooster, tvGpsBooster;
 	TextView tvAntiVirus, tvWifiExtender, tvBatterySaver;
 	Button btnContact, btnAboutUs;
 
+	String[] navItems;
+	
+	// Drawer variables
+	private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle = "Andro-Booster";
+    private CharSequence mTitle = "Andro-Booster";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+		
+		navItems = getResources().getStringArray(R.array.nav_items);
 		
 		initIDs();
 		initActions();
@@ -25,6 +46,10 @@ public class MainActivity extends Activity {
 
 	private void initIDs() {
 		// TODO Auto-generated method stub
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		
+		lvNavDrawer = (ListView) findViewById(R.id.left_drawer);
+		
 		tvMemoryBooster = (TextView) findViewById(R.id.textView_main_memoryBooster);
 		tvSignalBooster = (TextView) findViewById(R.id.textView_main_signalBooster);
 		tvGpsBooster = (TextView) findViewById(R.id.textView_main_gpsBooster);
@@ -39,6 +64,37 @@ public class MainActivity extends Activity {
 
 	private void initActions() {
 		// TODO Auto-generated method stub
+		 mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+	
+		lvNavDrawer.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navItems));
+		lvNavDrawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this, "Navigate to: " + navItems[arg2], Toast.LENGTH_SHORT).show();
+				mTitle = navItems[arg2];
+				// Highlight the selected item, update the title, and close the drawer
+			    lvNavDrawer.setItemChecked(arg2, true);
+			    mDrawerLayout.closeDrawer(lvNavDrawer);
+			}
+		});
+		
 		tvMemoryBooster.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -116,7 +172,41 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		
 		return true;
 	}
 
+	@Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(lvNavDrawer);
+        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        
+        return super.onPrepareOptionsMenu(menu);
+    }
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+        	
+          return true;
+        }
+        // Handle other action bar items here...
+
+        return super.onOptionsItemSelected(item);
+    }
+	
+	@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 }
